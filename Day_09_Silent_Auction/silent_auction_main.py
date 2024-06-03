e6a552404d6e150689f859284a1de7b025cab1f0
donny333 <125969039+donny333@users.noreply.github.com>
2024-06-03 23:10:12 +0300

    bidding logic created

--- Day_09_Silent_Auction/silent_auction_main.py
+++ Day_09_Silent_Auction/silent_auction_main.py
@@ -3,5 +3,22 @@ import os
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
+    print(logo)
 
-print(logo)
+clear()
+
+auction_over = False
+bidders_and_bids = {
+
+}
+
+while not auction_over:
+    bidder_name = input("What is your name?\n")
+    bid = int(input("What is your bid?\n"))
+    bidders_and_bids[bidder_name] = bid
+    last_bid = input("Are there more bidders? Type Y or N:\n")
+    if last_bid.lower() == 'n':
+        auction_over = True
+    clear()
+    print(bidders_and_bids)
+
