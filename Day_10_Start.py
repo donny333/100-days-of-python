6ad4a53244fd84e39f0c3becccb7fd773a630f70
donny333 <125969039+donny333@users.noreply.github.com>
2024-06-23 11:58:06 +0300

    docstring created for a fucntion

--- Day_10_Start.py
+++ Day_10_Start.py
@@ -1,6 +1,7 @@
 #Functions with Outputs
 
 def format_name(f_name, l_name):
+    """Take a first and last name and format it to return the title version of the name"""
     if f_name == "" or l_name == "":
         return "Please enter a valid input."
     formated_f_name = f_name.title()
