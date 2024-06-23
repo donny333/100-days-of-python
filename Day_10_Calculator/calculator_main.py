b3b05492ce53cfeb4fcd1bea90c8584e8063780f
donny333 <125969039+donny333@users.noreply.github.com>
2024-06-23 12:26:04 +0300

    basic calculator functions created

---
+++ Day_10_Calculator/calculator_main.py
@@ -0,0 +1,23 @@
+import os
+
+def clear():
+    """Clears all console screen."""
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+#Calculator
+    
+#Add
+def add(n1, n2):
+    return n1 + n2
+
+#Substract
+def sub(n1, n2):
+    return n1 + n2
+
+#Multiply
+def mult(n1, n2):
+    return n1 + n2
+
+#Divide
+def div(n1, n2):
+    return n1 + n2
