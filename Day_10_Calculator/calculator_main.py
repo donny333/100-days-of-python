ea1bde721bb0f80c1c14dc16faa0fc1256d38106
donny333 <125969039+donny333@users.noreply.github.com>
2024-06-23 13:36:11 +0300

    multiple actions possible

--- Day_10_Calculator/calculator_main.py
+++ Day_10_Calculator/calculator_main.py
@@ -36,7 +36,20 @@ for operation in operations:
     print(operation)
 operation_symbol = input("Pick an operation from the line above: ")
 num2 = int(input("What's the second number?: "))
-
 answer = operations[operation_symbol](num1, num2)
-
 print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+continue_calculations = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: " )
+recount_number = 0
+
+while continue_calculations.lower() == 'y':
+    if recount_number < 1:
+        last_answer = answer
+    else:
+        last_answer = new_answer
+    operation_symbol = input("Pick another operation: ")
+    num3 = int(input("What's the next number?: "))
+    new_answer = operations[operation_symbol](last_answer, num3)
+    print(f"{last_answer} {operation_symbol} {num3} = {new_answer}")
+    recount_number += 1
+    continue_calculations = input(f"Type 'y' to continue calculating with {new_answer}, or type 'n' to exit.: " )
